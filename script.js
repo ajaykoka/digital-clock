2f7c74ce426529cefff6c765f00d8572b76e2df8
ajaykoka <ajaykoka1704@gmail.com>
2021-11-17 16:23:23 +0530

    clock v1

---
+++ script.js
@@ -0,0 +1,62 @@
+//switch cases
+//print time
+//set interval
+//new date
+
+function printTime(){
+    //assigning variables
+    let d = new Date();
+    let hours =d.getHours();
+    let minutes =d.getMinutes();
+    let seconds =d.getSeconds();
+    let day = d.getDay();
+    let date =  d.getDate();
+    let month = d.getMonth();
+    let year = d.getFullYear();
+
+    // assigning days based on number using switch case
+    switch(day){
+        case 0:
+            day = "sunday"
+            break;
+        case 1:
+            day = "monday"
+            break;
+        case 2:
+            day = "tuesday"
+            break;
+        case 3:
+            day = "wednesday"
+            break;
+        case 4:
+            day = "Thursday"
+            break;
+        case 5:
+            day = "Friday"
+            break;
+        case 6:
+            day = "Saturday"
+            break;
+    }
+    
+    if(hours<10){
+        hours = "0"+ hours;
+    }
+    if(minutes<10){
+        minutes = "0"+ minutes;
+    }
+    if(seconds<10){
+        seconds = "0"+ seconds;
+    }
+
+    month = month + 1;
+    document.getElementById('clock').innerHTML = hours + ":" + minutes + ":" + seconds;
+    document.getElementById('day-date').innerHTML = day + "," + date + "-" + month + "-" + year;
+
+}
+
+window.onload =  function(){
+    printTime();
+}
+
+setInterval(printTime, 1000);
